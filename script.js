6fa8f86cc39502ce8cadc9dd49c0ef1363e71a1a
StanVreeland <stanvreeland@live.com>
2023-01-25 10:28:58 -0500

    continued creating grid through styles and js

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-xAxis = {};
-rows = 16;
-widthProperty = `${rows/100}%`;
+let yAxis = {};
+let rows = 16;
+let widthAndHeight = `${100/rows}%`;
+let cellNumber = 0;
+let brightnessDict = 1;
+
+let bodyRef = document.getElementById('body');
+let drawingWrapper = document.createElement('div');
+
+drawingWrapper.setAttribute('id', 'drawing-wrapper');
+
+bodyRef.appendChild(drawingWrapper);
 
 for (let i = 0; i < rows; i++) {
-
     // body.appendChild(document.createElement('div'))
 
-    xAxis[i] = document.createElement('div');
-    xAxis[i].setAttribute('class', 'parent-div');
-    xAxis[i].style.width = widthProperty;
-    for (let j = 0; j < 16; j++){
-        xAxis[i].appendChild('div');
-        xAxis[i][j].setAttribute('class', 'drawing-cell');
+    yAxis[i] = document.createElement('div');
+    yAxis[i].setAttribute('class', 'parent-div');
+    yAxis[i].style.height = widthAndHeight;
+    yAxis[i].style.width = '100vw';
+    for (let j = 0; j < rows; j++){
+        yAxis[i].appendChild(document.createElement('div'));
+        yAxis[i].childNodes[j].setAttribute('class', 'drawing-cell');
+        yAxis[i].childNodes[j].style.width = widthAndHeight;
+        yAxis[i].childNodes[j].style.height = '100%';
+        yAxis[i].childNodes[j].setAttribute('id', `${cellNumber}`);
+        yAxis[i].style.filter = `brightness(1)`;
+        brightnessDict[cellNumber] = 1;
+        cellNumber++;
+
+        yAxis[i].childNodes[j].addEventListener('click', () => {
+            yAxis[i].style.filter = `brightness(${brightnessDict[cellNumber]-0.75})`;
+            console.log(`click on ${cellNumber}`)   
+        });
     }
     
 
 }
 
-for (let i = 0; i < Object.keys(xAxis).length(); i++) {
-    body.appendChild(xAxis[i]);
+
+
+for (let i = 0; i < Object.keys(yAxis).length; i++) {
+    drawingWrapper.appendChild(yAxis[i]);
 }
 
 
 
-body = document.getElementById('body');
 
-console.log(widthProperty);
+
+console.log(Object.keys(yAxis).length);
